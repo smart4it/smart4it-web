25ce5e225d36cfbdcbf5ca85c04c7d17d6ca1c03
Василенко Илья <ivvasilen1@mts.ru>
2024-02-08 22:27:19 +0300

    Добавлена ссылка на дэшборт колличества вакансий

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -11,6 +11,9 @@
       <v-col>
         <v-btn to="/blog">Блог</v-btn>
         <v-btn to="/">Статистика</v-btn>
+        <v-btn href="http://185.178.45.174:3000/d/db7c78b0-857c-47fa-9e24-8b4e7b341d86/kolichestvo-java-vakansij?orgId=1&from=1675206825000&to=1707419774175&theme=light">
+          График количества вакансий
+        </v-btn>
         <v-btn to="/dictionary/experience">Справочники</v-btn>
       </v-col>
 
